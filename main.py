80b1bc052b5c0edf03c3b1e1da38a79f9af95656
Erin Bryson <bryson.erin.n@gmail.com>
2020-12-04 23:43:58 -0600

    Add iterative sheet traversing

--- main.py
+++ main.py
@@ -7,22 +7,22 @@
 #   DATE LAST MODIFIED: 12/04/2020
 #
 #   PROGRAM STEPS
+#   ) Set Current path
 #   ) Import the following from config.json:
 #       - data_dir:     Directory housing generated child-directories
 #       - report_name:  File name of generated report
 #       - report_ext:   File extension of generated report
-#       -
+#       - data_sheets:  Dictionary of sheets to use, each containing a list of required info
+#       - data_columns: List of required columns in extracted data
+#   ) Construct dir paths
 #   ) Get list of all generated child directories
 #   ) Create empty master DataFrame
 #   ) For each report in directory
 #   )   Create pandas ExcelFile object
-#   )   Create run_info DataFrame from sheet "Sheet1"
-#   )   Extract the following info:
-#       - Sample_Name
-#       - Date_and_time
-#       - Operator_Name3
-#       - Method_Name
-#   )   Create
+#   )   For each required sheet
+#   )       Create temp_data_df DataFrame from sheet
+#   )       Remove all but data specified in sheet dictionary
+#   )       Append data to master table
 #
 ########################################################################################################################
 
@@ -46,6 +46,7 @@ report_ext = config["report_ext"]                   # Set report file extension
 data_sheets = config["data_sheets"]                 # Get DataSheet dictionary
 data_columns = config["data_columns"]               # Set data columns for export
 
+# STEP : Construct Directory Paths
 data_dir_path = f"{curr_dir}\\{data_dir}"           # Construct data dir path
 report_file = f"{report_file_name}.{report_ext}"    # Construct report file
 
@@ -53,31 +54,20 @@ report_file = f"{report_file_name}.{report_ext}"    # Construct report file
 child_dirs = os.listdir(f"{data_dir_path}")
 
 # STEP : Create Empty Master DataFrame
-pass
+export_data_df = pd.DataFrame(columns=data_columns)
+
+print(export_data_df)
 
 # STEP : Access each directory
 for child_dir in child_dirs:
-    # Create file name
-    file_name = f"{data_dir_path}\\{child_dir}\\{report_file}"
+    file_name = f"{data_dir_path}\\{child_dir}\\{report_file}"  # Create file name
 
 # STEP : Create Excel File Object
     file = pd.ExcelFile(file_name)
 
-# STEP : Get run info data
-    run_info_df = file.parse(sheet_name="Sheet1", skiprows=1)
-    # A) Extract run info from run_info_df
-    # run_info_df.set_index(")
+# STEP : Extract data per sheet
+    for key in data_sheets:
+        sheet_cols = data_sheets[key]               # Get list of req columns in sheet
+        sheet_data_df = file.parse(sheet_name=key)   # Store sheet data in DataFrame
 
-    # print(run_info_df)
-    pass
-
-    # B) Transpose data
-    pass
-
-    # C) Add data to master table
-    pass
-
-# STEP : Get compound data
-    compound_df = file.parse(sheet_name="Compound")
-    # print(compound_df)
-    pass
+# STEP :
