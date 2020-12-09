32296566c0904af0a3e1186dc320a08eccd385ff
Erin Bryson <bryson.erin.n@gmail.com>
2020-12-08 19:37:48 -0600

    Add Config class to import config file settings

--- support/datasheet.py
+++ support/datasheet.py
@@ -1,5 +1,24 @@
 # IMPORTS
-import json
+import json as js
+
+
+# Config class
+class Config:
+    def __init__(self, file_name: str):
+        # Import Config File
+        with open("config.json") as config_file:
+            config = js.load(config_file)
+
+        # Set Data Directory
+        self.data_dir = config["data_dir"]
+
+        # Set Report Name
+        self.report_name = config["report_name"]
+
+        # If specified data sheets are listed, get data sheets
+        if "data_sheets" in config:
+            pass
+        pass
 
 
 # Class housing data sheet info
