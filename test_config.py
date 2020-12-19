cc6574ed9584bb21d97d967801fb3a2186665c5f
Erin Bryson <bryson.erin.n@gmail.com>
2020-12-19 17:51:51 -0600

    REFACTOR test module file name

--- test_config.py
+++ test_config.py
@@ -7,14 +7,27 @@
 #   DATE LAST MODIFIED: 12/04/2020
 #
 #   DESCRIPTION:
-#       This file is used to test the Config class
+#       This file is used to test the Config class in library file datasheet.py
 #
 ########################################################################################################################
 
+# IMPORTS
+import unittest as ut
 from support.datasheet import Config
 
-filename = "support\\config.json"
+# Set Test Variables
+config_file = "support\\config.json"    # Set filename
+test_data_dir = "Data_Directories"
+test_report_ext = "xls"
 
-config = Config(filename)
+config = Config(config_file)            # Set config object
 
-print(config)
+
+class TestConfigImports(ut.TestCase):
+    def test_data_dit(self):
+        self.assertTrue(config.report_ext, test_report_ext)
+
+
+if __name__ == '__main__':
+    ut.main()
+    pass
