e4692f773a17fed3541003406d57dbf25ac56caa
Erin Bryson <bryson.erin.n@gmail.com>
2021-01-21 21:48:25 -0600

    Add to all but import_data and clean_data tests.

--- support/test_datasheet_class.py
+++ support/test_datasheet_class.py
@@ -3,8 +3,6 @@ from support.data_upload import DataSheet
 import unittest as ut
 # import pandas as pd
 
-# --- TEST SHEET 1 --- #
-
 sheet1_metadata = {
         "headers_bool": 0,
         "transpose_bool": 1,
@@ -57,26 +55,38 @@ class TestDataSheet(ut.TestCase):
         pass
 
     def test_header(self):
-        # Expected Test Values
+        # Expected Values
         sheet1_headers = None
         compound_headers = 0
 
+        # Tests
         self.assertEqual(self.sheet_1.header, sheet1_headers)
         self.assertEqual(self.compound.header, compound_headers)
 
+    def test_headers_bool(self):
+        # Expected Values
+        sheet1_headers_bool = False
+        compound_headers_bool = True
+
+        # Tests
+        self.assertEqual(self.sheet_1.header_bool, sheet1_headers_bool)
+        self.assertEqual(self.compound.header_bool, compound_headers_bool)
+
     def test_transpose_bool(self):
-        # Correct Test Values
+        # Expected Values
         sheet1_transpose_bool = True
         compound_transpose_bool = True
 
+        # Tests
         self.assertEqual(self.sheet_1.transpose_bool, sheet1_transpose_bool)
         self.assertEqual(self.compound.transpose_bool, compound_transpose_bool)
 
     def test_use_cols(self):
-        # Correct Test Values
+        # Expected Values
         sheet1_use_cols = None
         compound_use_cols = ["Name", "Amount"]
 
+        # Tests
         self.assertEqual(self.sheet_1.use_cols, sheet1_use_cols)
         self.assertEqual(self.compound.use_cols, compound_use_cols)
         self.assertRaises(KeyError, self.compound_missing_cols_error.use_cols)
@@ -104,10 +114,22 @@ class TestDataSheet(ut.TestCase):
         self.assertEqual(self.compound.data_map, compound_data_map)
 
     def test_data_map_keys(self):
-        pass
+        # Expected Values
+        sheet1_data_map_keys = ["AcqMeth", "AcqOp", "InjDateTime", "SampleName"]
+        compound_data_map_keys = ["CARBON DIOXIDE", "HYDROGEN", "OXYGEN", "NITROGEN", "METHANE", "CARBON MONOXIDE"]
+
+        # Tests
+        self.assertEqual(self.sheet_1.data_map_keys, sheet1_data_map_keys)
+        self.assertEqual(self.compound.data_map_keys, compound_data_map_keys)
 
     def test_data_map_vals(self):
-        pass
+        # Expected Values
+        sheet1_data_map_vals = ["method_nm", "operator_nm", "dt_tm", "sample_nm"]
+        compound_data_map_vals = ["CO2_micro_L", "H2_micro_L", "O2_micro_L", "N2_micro_L", "CH4_micro_L", "CO_micro_L"]
+
+        # Tests
+        self.assertEqual(self.sheet_1.data_map_vals, sheet1_data_map_vals)
+        self.assertEqual(self.compound.data_map_vals, compound_data_map_vals)
 
     def test_import_data(self):
         pass
