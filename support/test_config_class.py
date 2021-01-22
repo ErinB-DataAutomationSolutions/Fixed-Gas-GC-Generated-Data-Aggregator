be714eb9b4d77a9842f6c73c0109a008322ba209
Erin Bryson <bryson.erin.n@gmail.com>
2021-01-21 21:45:15 -0600

    Update expected value for 'data_sheets'; move expected values from global to respective local scopes

--- support/test_config_class.py
+++ support/test_config_class.py
@@ -17,41 +17,64 @@ from support.data_upload import Config
 
 # Set Test Variables
 config_file = "../\\config_files\\test_config.json"
-data_dir = "Data_Directories"
-report_ext = "xls"
-data_sheets = {
-    "Sheet1": {
-        "AcqMeth":  "method_nm",
-        "AcqOp": "operator_nm",
-        "InjDateTime": "dt_tm",
-        "SampleName": "sample_nm"
-    },
-    "Compound":
-    [
-      "Amount"
-    ]
-  }
-
 
 class TestConfig(ut.TestCase):
 
     def setUp(self) -> None:
         self.config = Config(config_file)
-        print(type(self.config.data_sheets()))
 
     def tearDown(self) -> None:
         pass
 
     def test_data_dir(self):
+        # Expected Value
+        data_dir = "Data_Directories"
+
+        # Test
         self.assertEqual(self.config.data_dir, data_dir)
 
     def test_report_ext(self):
+        # Expected Value
+        report_ext = "xls"
+
+        # Test
         self.assertEqual(self.config.report_ext, report_ext)
 
     def test_is_dict(self):
         self.assertTrue(type(self.config.data_dir), dict)
 
     def test_data_sheets(self):
+        # Expected Value
+        data_sheets = {
+            "Sheet1": {
+                "headers_bool": 0,
+                "transpose_bool": 1,
+                "data_map": {
+                    "AcqMeth": "method_nm",
+                    "AcqOp": "operator_nm",
+                    "InjDateTime": "dt_tm",
+                    "SampleName": "sample_nm"
+                }
+            },
+            "Compound": {
+                "headers_bool": 1,
+                "headers_nm": [
+                    "Name",
+                    "Amount"
+                ],
+                "transpose_bool": 1,
+                "data_map": {
+                    "CARBON DIOXIDE": "CO2_micro_L",
+                    "HYDROGEN": "H2_micro_L",
+                    "OXYGEN": "O2_micro_L",
+                    "NITROGEN": "N2_micro_L",
+                    "METHANE": "CH4_micro_L",
+                    "CARBON MONOXIDE": "CO_micro_L"
+                }
+            }
+        }
+
+        # Test
         self.assertDictEqual(self.config.data_sheets(), data_sheets)
 
 
