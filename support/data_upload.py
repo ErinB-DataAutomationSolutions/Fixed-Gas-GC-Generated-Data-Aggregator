45af9fdfcd376375bd53aca0f7afd6a80fea0c81
Erin Bryson <bryson.erin.n@gmail.com>
2021-05-07 13:46:36 -0500

    Add ExportData class

--- support/data_upload.py
+++ support/data_upload.py
@@ -142,13 +142,16 @@ class DataSheet:
         return data_df
 
 
-class DataExport:
+class ExportData:
 
-    def __init__(self, export_dir: str, export_file_nm: str, export_file_ext: str, export_data):
+    def __init__(self, export_dir: str, export_file_nm: str, export_file_ext: str, export_data: pd.DataFrame):
         self.export_dir = export_dir
         self.export_file_nm = export_file_nm
         self.export_data = export_data
         self.export_file_ext = export_file_ext
+
+    def add_data_row(self, temp_data: pd.DataFrame):
+        self.export_data = pd.concat([self.export_data, temp_data])
 
     def export(self):
         self.export_data.to_excel(f"{self.export_file_nm}.{self.export_file_ext}")
