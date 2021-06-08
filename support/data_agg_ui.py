41fa4c5545dc227a3098176aaf1cb2dd6b45f597
Erin Bryson <bryson.erin.n@gmail.com>
2021-06-07 22:45:48 -0500

    Create UI file; set up unit test code

--- support/data_agg_ui.py
+++ support/data_agg_ui.py
@@ -1,12 +1,11 @@
 # IMPORTS:
 
+from tkinter import *
 from tkinter import filedialog, messagebox as mb
-
 import shutil
-
 import os
-
 import easygui
+# from PIL import Image, ImageTk
 
 
 def get_user_str_input(prompt_str):
@@ -54,11 +53,8 @@ class DAggUserInterface:
 
     def __init__(self):
         self.username = None
-
         self.config_file_path = None
-
         self.input_directory_path = None
-
         self.output_directory_path = None
 
     def get_username(self):
@@ -68,11 +64,41 @@ class DAggUserInterface:
         self.config_file_path = get_file_path()
 
     def get_input_directory_path(self):
-        self.output_directory_path = get_directory_path()
+        self.input_directory_path = get_directory_path()
 
     def get_output_directory_path(self):
         self.output_directory_path = get_directory_path()
 
+    def create_config_file(self):
+        pass
+
+    def aggregate_data(self):
+        pass
+
+    def export_data(self):
+        pass
+
 
 def initiate_gui():
-    pass
+
+    ui = DAggUserInterface()
+
+    root = Tk()
+    # creating a canvas to insert image
+    canv = Canvas(root, width=500, height=420, bg='white')
+    canv.grid(row=0, column=2)
+    # img = ImageTk.PhotoImage(Image.open("D:\\learn\\TechVidvan\\TechVidvan.png"))
+    canv.create_image(20, 20, anchor=NW)
+    # creating label and buttons to perform operations
+    Label(root, text="Fixed Gas GC Data Aggregator", font=("Helvetica", 16), fg="blue").grid(row=5, column=2)
+    Button(root, text="Open config file", command=ui.get_config_file_path).grid(row=15, column=2)
+    Button(root, text="Get Username", command=ui.get_username).grid(row=25, column=2)
+    Button(root, text="Get Input directory Path", command=ui.get_input_directory_path).grid(row=35, column=2)
+    Button(root, text="Get Output directory path", command=ui.get_output_directory_path).grid(row=45, column=2)
+    Button(root, text="Create Config File", command=ui.create_config_file).grid(row=55, column=2)
+    Button(root, text="Aggregate Data", command=ui.aggregate_data).grid(row=65, column=2)
+    Button(root, text="Export Data", command=ui.aggregate_data).grid(row=75, column=2)
+
+    root.mainloop()
+
+    return ui.config_file_path, ui.username, ui.input_directory_path, ui.output_directory_path
